ec876bb38af9eb0aa3180a326a1cd5148fa4ac04
Irina Stabrovskaya <foxy-54@mail.ru>
2022-12-30 13:26:08 +0800

    исправления после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,27 @@
 let popUp = document.querySelector('.pop-up');
 let openPopUpButton = document.querySelector('.profile__edit-btn');
 let closePopUpButton = document.querySelector('.pop-up__close-btn');
+let formElement = document.querySelector('.pop-up__form');
+let nameInput = document.querySelector('.pop-up__form-input-name');
+let jobInput = document.querySelector('.pop-up__form-input-job');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
 
 function openPopUp() {
   popUp.classList.add('pop-up_opened');
 }
+
 function closePopUp() {
   popUp.classList.remove('pop-up_opened');
 }
 
-openPopUpButton.addEventListener('click', openPopUp);
-closePopUpButton.addEventListener('click', closePopUp);
-
-
-
-
-let formElement = document.querySelector('.pop-up__form');
-
-let nameInput = document.querySelector('.pop-up__form-input-name');
-let jobInput = document.querySelector('.pop-up__form-input-job');
-
-
 function handleFormSubmit(evt) {
   evt.preventDefault();
-
-  let profileTitle = document.querySelector('.profile__title');
-  let profileSubtitle = document.querySelector('.profile__subtitle');
-
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
+  closePopUp();
 }
 
-
+openPopUpButton.addEventListener('click', openPopUp);
+closePopUpButton.addEventListener('click', closePopUp);
 formElement.addEventListener('submit', handleFormSubmit);
