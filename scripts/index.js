20b566bcafb96cb0ed0b14b1c45962ff09ed6c05
Irina Stabrovskaya <foxy-54@mail.ru>
2023-01-10 12:54:03 +0800

    коммит после правок второго ревью

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,19 @@ let popUp = document.querySelector('.pop-up');
 let openPopUpButton = document.querySelector('.profile__edit-btn');
 let closePopUpButton = document.querySelector('.pop-up__close-btn');
 let formElement = document.querySelector('.pop-up__form');
-let nameInput = document.querySelector('.pop-up__form-input-name');
-let jobInput = document.querySelector('.pop-up__form-input-job');
+let nameInput = document.querySelector('.pop-up__form-input_type_name');
+let jobInput = document.querySelector('.pop-up__form-input_type_job');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
+nameInput.getAttribute('value');
+jobInput.getAttribute('value');
+
 function openPopUp() {
   popUp.classList.add('pop-up_opened');
+
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
 }
 
 function closePopUp() {
